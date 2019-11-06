641a29acc4cab44753abcffad5df1c77db906d17
Peter <greenman@the-organization.com>
2019-11-06 17:10:11 +0000

    json path fix

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ var CONFIG = {
         "showDetail": true
     },
     "palermo":{
-        "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/polermo.geojson",
+        "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/palermo.geojson",
         "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
         "startView" : "map",
