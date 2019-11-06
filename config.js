99b26b319e12e1a06b6334c52375beabc7e063d6
Peter <greenman@the-organization.com>
2019-11-06 17:08:01 +0000

    config fix

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var CONFIG = {
         "startView" : "map",
         "showDetail": true
     },
-    "polermo":{
+    "palermo":{
         "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/polermo_en.geojson",
         "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
