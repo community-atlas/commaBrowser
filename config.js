8845705a394f015ca8ff0e7d298d6b07bc1ed20a
Peter <greenman@the-organization.com>
2019-11-25 17:55:07 +0000

    playground added

--- config.js
+++ config.js
@@ -4,7 +4,20 @@
  * 
  */
 
-var CONFIG = {    
+var CONFIG = {  
+    "playground" :{
+        "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
+        "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/play.geojson",        
+        "homepage": {
+            "url": "https://www.community-atlas.net",
+            "title": "community-atlas.net",
+        },
+        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapId": "mapbox.streets",
+        "startView" : "map",
+        "showDetail": true,       
+        "lang":"en"
+    },  
     "mehringplatz-en" :{
         "editorUrl": "https://coda.io/d/dQ3lMBnVcYw",
         "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/mehringplatz_en.geojson",        
