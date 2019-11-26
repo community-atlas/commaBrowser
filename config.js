3dc8881e3b1d999ab9bdc701bc99a3df6aa94d38
Peter <greenman@the-organization.com>
2019-11-26 12:49:22 +0000

    now using published github location

--- config.js
+++ config.js
@@ -5,22 +5,9 @@
  */
 
 var CONFIG = {  
-    "playground" :{
-        "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
-        "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/play-vdo5WMIQlF.geojson",        
-        "homepage": {
-            "url": "https://www.community-atlas.net",
-            "title": "community-atlas.net",
-        },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
-        "mapId": "mapbox.streets",
-        "startView" : "map",
-        "showDetail": true,       
-        "lang":"en"
-    },  
     "mehringplatz-en" :{
         "editorUrl": "https://coda.io/d/dQ3lMBnVcYw",
-        "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/mehringplatz_en-Q3lMBnVcYw.geojson",        
+        "commaJSONUrl" : "https://comensi.github.io/atlases/mehringplatz_en-Q3lMBnVcYw.geojson",        
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
@@ -34,9 +21,23 @@ var CONFIG = {
         },
         "lang":"en"
     },
+    "playground" :{
+        "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
+        "commaJSONUrl" : "https://comensi.github.io/atlases/play-vdo5WMIQlF.geojson",        
+        "homepage": {
+            "url": "https://www.community-atlas.net",
+            "title": "community-atlas.net",
+        },
+        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapId": "mapbox.streets",
+        "startView" : "map",
+        "showDetail": true,       
+        "lang":"en"
+    },  
+   
     "palermo":{
         "editorUrl": "https://coda.io/d/dWcfAWfG9io",        
-        "commaJSONUrl" : "https://raw.githubusercontent.com/Comensi/atlases/master/polermo-WcfAWfG9io.geojson",
+        "commaJSONUrl" : "https://comensi.github.io/atlases/polermo-WcfAWfG9io.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
