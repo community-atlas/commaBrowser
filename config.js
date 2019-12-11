0c1edd469a8c50bd9cc2112884181fb7f4eb51b0
Peter <greenman@the-organization.com>
2019-12-11 14:56:15 +0000

    Config clean

--- config.js
+++ config.js
@@ -4,150 +4,150 @@
  * 
  */
 
-var CONFIG = {  
-    "playground" :{
-    "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
-    "commaJSONUrl" : "https://comensi.github.io/atlases/play-vdo5WMIQlF.geojson",        
-    "homepage": {
-        "url": "https://www.community-atlas.net",
-        "title": "community-atlas.net",
-    },
-    "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
-    "mapId": "mapbox.streets",
-    "startView" : "map",
-    "showDetail": true,       
-    "lang":"en"
-},  
-    "mehringplatz-en" :{
-        "editorUrl": "https://coda.io/d/dQ3lMBnVcYw",
-        "commaJSONUrl" : "https://comensi.github.io/atlases/mehringplatz_en-Q3lMBnVcYw.geojson",        
+var CONFIG = {
+    "playground": {
+        "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
+        "commaJSONUrl": "https://comensi.github.io/atlases/play-vdo5WMIQlF.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
+        "startView": "map",
+        "showDetail": true,
+        "lang": "en"
+    },
+    "mehringplatz-en": {
+        "editorUrl": "https://coda.io/d/dQ3lMBnVcYw",
+        "commaJSONUrl": "https://comensi.github.io/atlases/mehringplatz_en-Q3lMBnVcYw.geojson",
+        "homepage": {
+            "url": "https://www.community-atlas.net",
+            "title": "community-atlas.net",
+        },
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapId": "mapbox.streets",
+        "startView": "map",
         "showDetail": true,
         "filters": {
-            "category":["Delimitations"],
+            "category": ["Delimitations"],
         },
-        "lang":"en"
+        "lang": "en"
     },
-    "mehringplatz-de" :{
+    "mehringplatz-de": {
         "editorUrl": "https://coda.io/d/daBTKgiA4dJ",
-        "commaJSONUrl" : "https://comensi.github.io/atlases/mehringplatz_de-aBTKgiA4dJ.geojson",        
+        "commaJSONUrl": "https://comensi.github.io/atlases/mehringplatz_de-aBTKgiA4dJ.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
-        "showDetail": true,       
-        "lang":"de"
+        "startView": "map",
+        "showDetail": true,
+        "lang": "de"
     },
 
 
-    "atlas" :{
+    "atlas": {
         "editorUrl": "https://coda.io/d/db8UloKHtPW",
-        "commaJSONUrl" : "https://comensi.github.io/atlases/atlas-b8UloKHtPW.geojson",
+        "commaJSONUrl": "https://comensi.github.io/atlases/atlas-b8UloKHtPW.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
-        "showDetail": true,       
-        "lang":"en"
+        "startView": "map",
+        "showDetail": true,
+        "lang": "en"
     },
-    "mildmay" :{
+    "mildmay": {
         "editorUrl": "https://coda.io/d/d8vrMUsemgn",
-        "commaJSONUrl" : "https://comensi.github.io/atlases/mildmay-8vrMUsemgn.geojson",
+        "commaJSONUrl": "https://comensi.github.io/atlases/mildmay-8vrMUsemgn.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
-        "showDetail": true,       
-        "lang":"en"
+        "startView": "map",
+        "showDetail": true,
+        "lang": "en"
     },
-  
-    "playground" :{
+
+    "playground": {
         "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
-        "commaJSONUrl" : "https://comensi.github.io/atlases/play-vdo5WMIQlF.geojson",        
+        "commaJSONUrl": "https://comensi.github.io/atlases/play-vdo5WMIQlF.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
-        "showDetail": true,       
-        "lang":"en"
-    },  
-    "savskonaselje" :{
+        "startView": "map",
+        "showDetail": true,
+        "lang": "en"
+    },
+    "savskonaselje": {
         "editorUrl": "https://coda.io/d/dYR6hsmoGmL",
-        "commaJSONUrl" : "https://comensi.github.io/atlases/savskonaselje-YR6hsmoGmL.geojson",
+        "commaJSONUrl": "https://comensi.github.io/atlases/savskonaselje-YR6hsmoGmL.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
-        "showDetail": true,       
-        "lang":"en"
-    },  
-    "bairrodaajuda" :{
+        "startView": "map",
+        "showDetail": true,
+        "lang": "en"
+    },
+    "bairrodaajuda": {
         "editorUrl": "https://coda.io/d/rMkxapY_KF.geojson",
-        "commaJSONUrl" : "https://comensi.github.io/atlases/barriodaajuda-rMkxapY_KF.geojson",
+        "commaJSONUrl": "https://comensi.github.io/atlases/barriodaajuda-rMkxapY_KF.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
-        "showDetail": true,       
-        "lang":"pt"
-    },  
-    
-   
-   
-   
-    "palermo":{
-        "editorUrl": "https://coda.io/d/dRf4FQINsE-",        
-        "commaJSONUrl" : "https://comensi.github.io/atlases/palermo-Rf4FQINsE-.geojson",
+        "startView": "map",
+        "showDetail": true,
+        "lang": "pt"
+    },
+
+
+
+
+    "palermo": {
+        "editorUrl": "https://coda.io/d/dRf4FQINsE-",
+        "commaJSONUrl": "https://comensi.github.io/atlases/palermo-Rf4FQINsE-.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
+        "startView": "map",
         "showDetail": true,
         "lang": "it"
     },
-    "testing" :{
-        "commaJSONUrl" : "https://raw.githubusercontent.com/community-atlas/commaBrowser/master/browser/tests/data/test1.geojson",
+    "testing": {
+        "commaJSONUrl": "https://raw.githubusercontent.com/community-atlas/commaBrowser/master/browser/tests/data/test1.geojson",
         "homepage": {
             "url": "https://www.community-atlas.net",
             "title": "community-atlas.net",
         },
-        "mapBoxToken" : "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox.streets",
-        "startView" : "map",
+        "startView": "map",
         "showDetail": true,
         "filters": {
-            "category":["Delimitations"],
+            "category": ["Delimitations"],
         },
-        "lang":"en"
+        "lang": "en"
     },
-  
- 
- 
+
+
+
 }
