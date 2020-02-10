d1cf08ec284b2bdb3433b580a6fdc17786be7673
Peter <greenman@the-organization.com>
2020-02-10 13:55:20 +0000

    fixed savskonaselje

--- config.js
+++ config.js
@@ -291,6 +291,52 @@ var CONFIG = {
             }
         ]
     },
+    "savskonaselje": {
+        "editorUrl": "https://coda.io/d/YR6hsmoGmL",
+        "commaJSONUrl": "https://comensi.github.io/atlases/savskonaselje-YR6hsmoGmL.geojson",   
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapId": "mapbox.streets",
+        "startView": "map",
+        "showDetail": true,
+        "lang": "it",
+        "logos" : [
+            {
+               src: "browser/images/logo-erasmusleft_en.jpg",
+               alt: "Erusmus",
+               href: null,
+            },
+            {
+                src: "browser/images/logo-comensi-darkandtransparent.png",
+                alt: "Comensi",
+                href: null,
+            },
+            {  
+                src: "browser/images/logo_bond.png",
+                alt: "Bond of Union",
+                href: "http://www.bondofunion.eu/"
+            },
+            {
+                src: "browser/images/logo-4change.png",
+                alt: "4Change",
+                href: "https://www.4change.org/"
+            },
+            {
+                src: "browser/images/logo-tesserae.jpg",
+                alt: "Tesserae",
+                href: "http://www.tesserae.eu/"
+            },
+            {
+                src: "browser/images/logo-prostoroz.jpg",       
+                alt: "prostoRož",
+                href: "http://prostoroz.org/en/",
+            },
+            {     
+                src: "browser/images/logo-mapping.png",
+                alt: "Mapping for Change",
+                href: "http://mappingforchange.org.uk/"
+            }
+        ]
+    },
     "testing": {
         "commaJSONUrl": "https://raw.githubusercontent.com/community-atlas/commaBrowser/master/browser/tests/data/test1.geojson",  
         "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
